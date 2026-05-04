1ffc8f65cb7baf411e3816df2515d087adb49910
ALex <alexei.gor.2006@google.com>
2026-05-04 12:35:30 +0300

    Switch to PostgreSQL

--- cometa/settings.py
+++ cometa/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dev-secret-change-in-production'
@@ -46,10 +48,9 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3')
+    )
 }
 
 AUTH_USER_MODEL = 'inventory.User'
